881f9b6e70fcf30ccafc86e9e641fbea076dae95
Edgar <edgaras.liberis@gmail.com>
2019-09-14 19:57:22 +0100

    Add working set plot generation

--- tflite_tools.py
+++ tflite_tools.py
@@ -14,6 +14,8 @@ def main():
     parser.add_argument("--optimize", action="store_true", default=False, help="optimize peak working set size")
     parser.add_argument("--csv", type=str, dest="csv_output_folder", default=None,
                         help="output model analysis in CSV format into the specified folder")
+    parser.add_argument("--plot", type=str, dest="plot_file", default=None,
+                        help="plot memory usage for each operator during the execution")
     args = parser.parse_args()
 
     # Example API usage:
@@ -26,6 +28,7 @@ def main():
         model.optimize_memory()
 
     if args.csv_output_folder:
+        print(f"Writing model analysis to {args.csv_output_folder} in CSV format")
         os.makedirs(args.csv_output_folder, exist_ok=True)
         model.output_model_analysis_to_csv(args.csv_output_folder)
     else:
@@ -33,6 +36,10 @@ def main():
 
     if args.clusters > 0:
         model.cluster_weights(args.clusters)
+
+    if args.plot_file:
+        print(f"Plotting operator memory usage to {args.plot_file}")
+        model.plot_memory_usage(args.plot_file)
 
     # Example API usage:
     # `model.evaluate(<data_iterator>)`
--- tflite_tools/tflite_model.py
+++ tflite_tools/tflite_model.py
@@ -13,10 +13,9 @@ import numpy as np
 from tqdm import tqdm
 from prettytable import PrettyTable
 
-from sklearn import cluster
-
 
 def cluster_weights(weights, n_clusters):
+    from sklearn import cluster
     kmeans = cluster.KMeans(n_clusters=n_clusters).fit(weights.reshape((-1, 1)))
     return kmeans.labels_.reshape(weights.shape), np.around(kmeans.cluster_centers_).astype(np.int32)
 
@@ -317,6 +316,59 @@ class TFLiteModel:
         print("Tensor information (weights excluded):")
         print(x)
         print()
+
+    def plot_memory_usage(self, plot_file):
+        """
+        Plots memory usage for each operator in the schedule as a stacked bar chart.
+        :param plot_file: Output file
+        """
+        import matplotlib.pyplot as plt
+
+        labels = []
+        input_sizes = []
+        output_sizes = []
+        other_sizes = []
+
+        schedule = self._execution_schedule_info()
+        peak_mem_use = 0
+
+        for item in schedule:
+            op, working_set, mem_use = item
+
+            input_size = TFLiteModel._cum_tensor_sizes(op.inputs)
+            output_size = op.output.size
+            other_size = TFLiteModel._cum_tensor_sizes(t for t in working_set if t not in op.inputs and t != op.output)
+
+            assert input_size + output_size + other_size == mem_use
+            peak_mem_use = max(peak_mem_use, mem_use)
+
+            labels.append(op.output.name)
+            input_sizes.append(input_size)
+            output_sizes.append(output_size)
+            other_sizes.append(other_size)
+
+        input_sizes = np.array(input_sizes) / 1024
+        output_sizes = np.array(output_sizes) / 1024
+        other_sizes = np.array(other_sizes) / 1024
+        peak_mem_use /= 1024
+
+        fig = plt.figure(figsize=(len(labels) / 3.5, 8))
+        plt.tight_layout()
+        ax = fig.gca()
+        x = np.arange(0, len(labels))
+
+        ax.bar(x, input_sizes, color="#D95319", label="Operator inputs")
+        ax.bar(x, output_sizes, bottom=input_sizes, color="#EDB120", label="Operator outputs")
+        ax.bar(x, other_sizes, bottom=(input_sizes + output_sizes), color="#0072BD", label="Other tensors")
+
+        ax.set_xticks(x)
+        ax.set_xlabel('Operators')
+        ax.set_ylabel('Memory usage (KB)')
+        ax.set_ylim([0, peak_mem_use + 10])
+        ax.set_xticklabels(labels, rotation=90)
+        ax.legend()
+
+        plt.savefig(plot_file, bbox_inches='tight', dpi=300)
 
     def _output_tensor_details_to_csv(self, csv_file):
         if not self.model_graph:
